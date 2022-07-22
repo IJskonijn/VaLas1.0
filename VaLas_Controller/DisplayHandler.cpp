de2b25b856a8d13c378fae1eb8263f3c858dc579
Gerben van der Laan <gerben.van-der-laan@nl.abb.com>
2022-07-22 16:06:22 +0200

    Small update

--- VaLas_Controller/DisplayHandler.cpp
+++ VaLas_Controller/DisplayHandler.cpp
@@ -8,20 +8,15 @@
 // 128x64 for 0.96" OLED
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 const char* stringToDisplayBuffer;
-VaLas_Controller::GearLeverPosition tempLeverPosition;
-int tempAtfTemp;
 
 DisplayHandler::DisplayHandler()
 {
   u8g2.begin();
-
-  tempLeverPosition = VaLas_Controller::GearLeverPosition::Unknown;
-  tempAtfTemp = 0;
 }
 
 void DisplayHandler::DisplayOnScreen(const char* stringToDisplay)
 {
-  DisplayOnScreen(stringToDisplay, tempLeverPosition, -1);
+  DisplayOnScreen(stringToDisplay, VaLas_Controller::GearLeverPosition::Unknown, -1);
 }
 
 void DisplayHandler::DisplayOnScreen(const char* stringToDisplay, VaLas_Controller::GearLeverPosition currentLeverPosition, int atfTemp)
@@ -39,19 +34,20 @@ void DisplayHandler::DisplayOnScreen(const char* stringToDisplay, VaLas_Controll
   u8g2.drawStr(1, 29, stringToDisplayBuffer);
 
   // Draw ATF temp
-  if (currentLeverPosition != VaLas_Controller::GearLeverPosition::Park && currentLeverPosition != VaLas_Controller::GearLeverPosition::Neutral)
+  if (currentLeverPosition != VaLas_Controller::GearLeverPosition::Unknown)
   {
-    if (atfTemp > -1)
-      atfTempToDisplay = String(atfTemp);
-    else
-      atfTempToDisplay = tempAtfTemp;
+    if (currentLeverPosition != VaLas_Controller::GearLeverPosition::Park && currentLeverPosition != VaLas_Controller::GearLeverPosition::Neutral)
+    {
+      if (atfTemp > -1)
+        atfTempToDisplay = String(atfTemp);
+    }
+
+    String tempVar = "ATF: " + atfTempToDisplay;// + String(" C");
+    u8g2.setFont(u8g2_font_logisoso18_tr);
+    u8g2.drawStr(10, 65, tempVar.c_str());
   }
 
-  String tempVar = "ATF: " + atfTempToDisplay;// + String(" C");
-  u8g2.setFont(u8g2_font_logisoso18_tr);
-  u8g2.drawStr(10, 65, tempVar.c_str());
   u8g2.sendBuffer();
-
   delay(150);
 }
 
